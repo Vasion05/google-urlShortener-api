d3699adf1262a85b5dfc11cdb91de52a12a5fdb0
Vasion05 <vasion.cse@gmail.com>
2017-06-08 12:46:19 +0600

    third commit

--- index.php
+++ index.php
@@ -2,45 +2,54 @@
 <html>
 <head>
     <title>Google API URL Shortener</title>
+    <style>
+        .google-api{width:400px;margin:auto;padding:10px 30px 30px;background:#ddd;box-shadow: 3px 3px 5px #888;}
+        .input-tbl{padding: 20px 0;}
+        input[type="submit"]{padding: 4px 20px;margin-top: 5px;cursor: pointer;}
+        input[type="text"]{padding: 4px 5px;}
+    </style>
 </head>
 <body>
+    <div class="google-api">
+        <h2>Google API URL Shortener</h2>
+        <?php
 
-    <?php
-    if(!empty($_POST['url'])){
+            if(!empty($_POST['url'])){
 
-        $longUrl = $_POST['url'];
+                $longUrl = $_POST['url'];
 
-        $apiKey  = 'AIzaSyBw8s-s-aSiPA9nLtuuuzyXhMcWSrBBee8'; 
-        
-        $postData = array('longUrl' => $longUrl);
-        $jsonData = json_encode($postData);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url?key='.$apiKey);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-        $response = curl_exec($ch);
-        $json = json_decode($response);
+                $apiKey  = 'AIzaSyBw8s-s-aSiPA9nLtuuuzyXhMcWSrBBee8'; 
+                
+                $postData = array('longUrl' => $longUrl);
+                $jsonData = json_encode($postData);
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url?key='.$apiKey);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+                $response = curl_exec($ch);
+                $json = json_decode($response);
 
-        curl_close($ch);
-        echo '<h4>Shortened URL : '.$json->id.'</h4>';
-    }
-    ?>
+                curl_close($ch);
+                echo '<h4>Shortened URL : <u>'.$json->id.'</u></h4>';
+            }
 
-    <form action="" method="post" accept-charset="utf-8">
-        <table>
-            <tr>
-                <td>Input LongURL: </td>
-                <td><input type="text" name="url"></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" value="Submit"></td>
-            </tr>
-        </table>
-    </form>
+        ?>
+        <form action="" method="post" accept-charset="utf-8">
+            <table class="input-tbl">
+                <tr>
+                    <td>Input LongURL: </td>
+                    <td><input type="text" name="url"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" value="Submit"></td>
+                </tr>
+            </table>
+        </form>
+    </div>
 </body>
 </html>
